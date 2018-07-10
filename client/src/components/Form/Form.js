f8b9f1a123caf2d2ca4b16b931a8a88be13a45d9
vanhooktristan <vanhooktristan@gmail.com>
2018-07-10 00:22:29 -0500

    updates to form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -11,33 +11,33 @@ const Form = props => (
         id="topic"
         type="text"
         value={props.q}
-        placeholder="Net Neutrality"
+        placeholder="Soccer"
         name="q"
         onChange={props.handleInputChange}
         required
       />
       <label htmlFor="start-year">
-        <strong>Start Year</strong>
+        <strong>Location</strong>
       </label>
       <input
         className="form-control"
         id="start-year"
         type="number"
         value={props.start_year}
-        placeholder="2017"
+        placeholder="Swope Park, Kansas City"
         name="start_year"
         onChange={props.handleInputChange}
         required
       />
       <label htmlFor="end-year">
-        <strong>End Year</strong>
+        <strong># of players</strong>
       </label>
       <input
         className="form-control"
         id="end-year"
         type="number"
         value={props.end_year}
-        placeholder="2018"
+        placeholder="5"
         name="end_year"
         onChange={props.handleInputChange}
         required
