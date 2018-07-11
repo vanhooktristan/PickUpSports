8d00babf01c88bc56101a087ac3dd6656112ef36
vanhooktristan <vanhooktristan@gmail.com>
2018-07-11 14:51:57 -0500

    merging both versions of app

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,42 +1,37 @@
 const db = require("../models");
 
-// Defining methods for the articleController
+// Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
-    db.Article
+    db.Game
       .find(req.query)
       .sort({ date: -1 })
-      .then(dbArticle => res.json(dbArticle))
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Article
+    db.Game
       .findById(req.params.id)
-      .then(dbArticle => res.json(dbArticle))
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    const article = {
-      _id: req.body._id,
-      title: req.body.headline.main,
-      url: req.body.web_url
-    };
-    db.Article
-      .create(article)
-      .then(dbArticle => res.json(dbArticle))
+    db.Game
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Article
+    db.Game
       .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbArticle => res.json(dbArticle))
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Article
+    db.Game
       .findById({ _id: req.params.id })
-      .then(dbArticle => dbArticle.remove())
-      .then(dbArticle => res.json(dbArticle))
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
 };
